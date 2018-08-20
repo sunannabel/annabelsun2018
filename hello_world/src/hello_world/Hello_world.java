e32f2eeab0c91c339107c77bf21c9455e6a1f62a
APCS2nd <APCS2nd@MHS-Math2-25>
2018-08-20 09:46:32 -0700

    first project to commit to github 2018. hello world

---
+++ hello_world/src/hello_world/Hello_world.java
@@ -0,0 +1,14 @@
+/**
+ * Print out a message saying Hello~
+ * 
+ * @author Annabel Sun
+ * @version 8/17/2018
+ */
+
+public class Hello_world 
+{	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World! From Annabel"); //Display the string
+	}
+}
