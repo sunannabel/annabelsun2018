fd9b810c2ff25651a5cdc785419cfeec47c161d3
APCS2nd <APCS2nd@MHS-Math2-25>
2018-08-31 09:50:25 -0700

    wrote pikachu code to print a pikachu

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,21 @@
+/**
+ * Prints this message:
+ * 
+ * Pikachu welcomes you to the world of Pokemon!
+ *              (\__/)
+ *              (o^.^)
+ *            z(_(")(")
+ *
+ * @author Annabel Sun
+ * @version 8/31/18
+ */
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("             (\\__/)");
+		System.out.println("             (o^.^)");
+		System.out.println("           z(_(\")(\")");
+	}
+
+}
